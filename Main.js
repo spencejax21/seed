2f96e090850c398a973a4515151f84c0d35cef1e
gsilver321 <jgb9fz@virginia.edu>
2023-03-26 01:34:33 -0400

    Commit for merges

--- Main.js
+++ Main.js
@@ -1,6 +1,7 @@
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import Profile from './profile/Profile';
 import Home from './home/Home';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function Main(){
 
@@ -8,7 +9,7 @@ export default function Main(){
 
     return (
       <Tab.Navigator>
-        <Tab.Screen name="home" component={Home} />
+        <Tab.Screen name="home" component={Home} options={{tabBarIcon: <Ionicons name="home" size={36} color="black" alignItems="right"/>}}/>
         <Tab.Screen name="profile" component={Profile} />
       </Tab.Navigator>
     );
