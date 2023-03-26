3df70fe286b2a5e06d06f6d743fea60680d5831a
gsilver321 <jgb9fz@virginia.edu>
2023-03-25 21:58:10 -0400

    Profile page and centering

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Home from './home/Home';
 import {NavigationContainer} from '@react-navigation/native';
 import Login from './login/Login';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Profile from './profile/Profile';
 
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
               <Stack.Navigator initialRouteName='login' screenOptions={{headerShown: false }}>
                 <Stack.Screen name="login" component={Login} />
                 <Stack.Screen name="home" component={Home} />
+                <Stack.Screen name="profile" component={Profile}/>
               </Stack.Navigator>
           </NavigationContainer>
         </NativeBaseProvider>
