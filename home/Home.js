fec3825d06d9bd8d9552aebe0309798583bb64c7
gsilver321 <jgb9fz@virginia.edu>
2023-03-26 06:26:01 -0400

    Added text to categories

--- home/Home.js
+++ home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconButton, FlatList, Icon, NativeBaseProvider, Box, Center, Heading, Stack, HStack, Text, Pressable, Fab} from 'native-base';
+import { IconButton, FlatList, Icon, NativeBaseProvider, Box, Center, Heading, Stack, HStack, Text, Pressable, Fab, ZStack} from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
 import { StyleSheet, View } from 'react-native'
 import Ionicons from '@expo/vector-icons/Ionicons';
@@ -41,18 +41,23 @@ const AppDrawer = ({navigation}) => {
       bottom: "40%",
       label: "All tasks"
     }];
-    return <FlatList scrollEnabled="false" numColumns={2} m={'-4px'} data={icons} renderItem={({
-      item
-    }) => {
-      return <IconButton label={item.label} m={'12px'} borderRadius="md" bg={item.bg} variant="solid" 
-          p="12" icon={<Icon color="white" name={item.name} as={MaterialIcons} size="4xl" aria-label={item.type}/>} 
+    return <ZStack paddingRight="80%">
+      <Text style={{zIndex: 1}} bottom="-100" right="208" color="white" fontSize="lg" bold="true">House</Text>
+      <Text style={{zIndex: 1}} bottom="-100" right="33" color="white" fontSize="lg" bold="true">Transport</Text>
+      <Text style={{zIndex: 1}} bottom="-230" right="202" color="white" fontSize="lg" bold="true">Recycle</Text>
+      <Text style={{zIndex: 1}} bottom="-230" right="43" color="white" fontSize="lg" bold="true">All lists</Text>
+      <FlatList scrollEnabled="false" numColumns={2} m={'-4px'} data={icons} renderItem={({
+      item }) => {
+      return <IconButton m={'12px'} borderRadius="md" bg={item.bg} variant="solid" 
+          p="12" icon={<Icon mt="-7" color="white" name={item.name} as={MaterialIcons} size="4xl" aria-label={item.type}/>} 
           onPress={() => {
             navigation.navigate('detail', {
             type: item.type,
             });
-          }} /> 
-    }} />;
-  }
+          }} />
+    }} /> 
+    </ZStack>
+  } 
 
   function Favorites() {
     return <Stack space={2}>
@@ -73,7 +78,6 @@ const AppDrawer = ({navigation}) => {
         </Box>
         <Box paddingTop="35%">
           <AppDrawer navigation={navigation}/>
-          <Favorites />
         </Box>
       </Box>
     </View>
